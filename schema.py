b64e715bc28047f75d99f1ec0ecd5bd85889f87c
haretaro <sei.haretaro.takahashi@gmail.com>
2017-11-16 10:40:13 +0900

    add schema.py

---
+++ schema.py
@@ -0,0 +1,14 @@
+#スキーマ表示するだけ
+from pyspark.sql import SparkSession
+
+spark = SparkSession\
+        .builder\
+        .appName('niconico')\
+        .config('master', 'yarn')\
+        .getOrCreate()
+
+meta = spark.read.json('/user/seitaro-t/meta/0001.jsonl')
+meta.printSchema()
+
+comment = spark.read.json('/user/seitaro-t/comment/0001/sm19961.jsonl')
+comment.printSchema()
